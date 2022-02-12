605a800ce2ca8a11dd4f840af842bf132b43c287
Jay Rovacsek <jay@rovacsek.com>
2022-02-13 00:34:50 +1100

    Add redshift

---
+++ services/redshift/default.nix
@@ -0,0 +1,15 @@
+{
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "1";
+    };
+    location.latitude = -32.917;
+    location.longitude = 151.8;
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
+}
