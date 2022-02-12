2fa15a611c03a17b83c8c5bd33cdb6a42f8b40b4
Jay Rovacsek <jay@rovacsek.com>
2022-02-13 00:35:25 +1100

    Minor fixes to redshift

--- services/redshift/default.nix
+++ services/redshift/default.nix
@@ -1,12 +1,15 @@
 {
   services.redshift = {
     enable = true;
+
+    latitude = -32.917;
+    longitude = 151.8;
+
     brightness = {
       day = "1";
       night = "1";
     };
-    location.latitude = -32.917;
-    location.longitude = 151.8;
+
     temperature = {
       day = 5500;
       night = 3700;
