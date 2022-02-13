64af1a220d2b987d1c55f2096dcd36813cceb52d
Jay Rovacsek <jay@rovacsek.com>
2022-02-13 14:05:13 +1100

    Add patches to module

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -1,10 +1,15 @@
-{
+{ config, lib, ... }:
+let patchDriver = import ./nvenc-unlock.nix;
+in {
   hardware = {
     opengl = {
       enable = true;
       driSupport32Bit = true;
     };
-    nvidia = { modesetting.enable = true; };
+    nvidia = {
+      package = patchDriver config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+    };
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
