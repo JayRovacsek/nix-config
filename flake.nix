3eff8e20137eac43996f668787a4ec2e7652c9a3
Jay Rovacsek <jay@rovacsek.com>
2022-02-15 00:01:40 +1100

    Remove scoped values

--- flake.nix
+++ flake.nix
@@ -12,48 +12,45 @@
     nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, darwin, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-        overlays = [ nur.overlay ];
-      };
-    in {
-      nixosConfigurations = {
-        alakazam = nixpkgs.lib.nixosSystem {
-          inherit pkgs system;
-          modules = [
-            ./hosts/alakazam
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.jay = import ./packages/linux-x86.nix;
-            }
-          ];
+  outputs = { self, nixpkgs, home-manager, nur, darwin, ... }@inputs: {
+    nixosConfigurations = {
+      alakazam = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+          overlays = [ nur.overlay ];
         };
-      };
-
-      darwinConfigurations = {
-        cloyster = darwin.lib.darwinSystem {
-          system = "x86_64-darwin";
-          pkgs = import nixpkgs {
-            inherit system;
-            config = { allowUnfree = true; };
-            overlays = [ nur.overlay ];
-          };
-          modules = [
-            ./hosts/cloyster
-            home-manager.darwinModule
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.jrovacsek = import ./packages/darwin_x86.nix;
-            }
-          ];
-        };
+        modules = [
+          ./hosts/alakazam
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jay = import ./packages/linux-x86.nix;
+          }
+        ];
       };
     };
+
+    darwinConfigurations = {
+      cloyster = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        pkgs = import nixpkgs {
+          system = "x86_64-darwin";
+          config = { allowUnfree = true; };
+          overlays = [ nur.overlay ];
+        };
+        modules = [
+          ./hosts/cloyster
+          home-manager.darwinModule
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jrovacsek = import ./packages/darwin_x86.nix;
+          }
+        ];
+      };
+    };
+  };
 }
