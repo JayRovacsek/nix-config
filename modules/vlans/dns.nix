80842d6b97037d82f9dae4eff54a169f60f5b2c6
Jay Rovacsek <jay@rovacsek.com>
2022-02-27 22:23:16 +1100

    Fix VLAN conf?

--- modules/vlans/dns.nix
+++ modules/vlans/dns.nix
@@ -1,6 +1,10 @@
 { interface ? "eth0", ... }: {
-  networking.vlans.dns = {
-    inherit interface;
-    id = 6;
+  networking = {
+    vlans.dns = {
+      inherit interface;
+      id = 6;
+    };
+    interfaces.dns.useDHCP = true;
+    defaultGateway = "192.168.${vlans.dns.id}.1";
   };
 }
