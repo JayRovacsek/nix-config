ddce231e7eb1c79492687110d5934d09241f765a
Jay Rovacsek <jay@rovacsek.com>
2022-03-07 09:21:21 +1100

    Split docker configs by arch

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -1,12 +1,7 @@
 { pkgs, ... }:
-let configs = { aarch64-linux = import ./aarch64-linux.nix; };
-in {
-  virtualisation.docker = if pkgs.system != "x86_64-linux" then
-    configs.${pkgs.system}
-  else {
-    enable = true;
-    enableNvidia = true;
-    rootless.enable = true;
-    autoPrune.enable = true;
+let
+  configs = {
+    aarch64-linux = import ./aarch64-linux.nix;
+    x86_64-linux = import ./x86_64-linux.nix;
   };
-}
+in { virtualisation.docker = configs.${pkgs.system}; }
