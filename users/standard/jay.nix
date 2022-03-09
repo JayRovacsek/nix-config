591e5330fc9388fcdcbdbc8a96bbaf17b914a954
Jay Rovacsek <jay@rovacsek.com>
2022-03-09 15:34:37 +1100

    Add new authorised key

--- users/standard/jay.nix
+++ users/standard/jay.nix
@@ -6,5 +6,6 @@
     [ "wheel" "docker" "libirt" "networkmanager" "audio" "video" "input" ];
   openssh.authorizedKeys.keys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAHcp9K2S8UZWpWTkuXLsEJQ57qoXpriEqHmU4AjrWKFAAAABHNzaDo= jay@rovacsek.com"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICOGbb7QMU4l5jccNIaGp9M8HBhyGm3JeNnD30td49nfAAAABHNzaDo= jay@rovacsek.com"
   ];
 }
