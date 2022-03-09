911862d8144ac47af3ebfd9ca730c31d97e16bcb
Jay Rovacsek <jay@rovacsek.com>
2022-03-09 22:55:18 +1100

    Rework volume mounts

--- modules/docker/configs/pihole.nix
+++ modules/docker/configs/pihole.nix
@@ -2,11 +2,8 @@ rec {
   image = "pihole/pihole:latest";
   serviceName = "pihole";
   ports = [ "53:53/tcp" "53:53/udp" "67:67/udp" "80:80/tcp" "444:443/tcp" ];
-  volumes = [
-    "/etc/pihole/pihole:/etc/pihole:rw"
-    "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw"
-    "/etc/pihole/config:/config:rw"
-  ];
+  volumes =
+    [ "/etc/pihole:/etc/pihole:rw" "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw" ];
   environment = {
     TZ = "Australia/Sydney";
     PIHOLE_DNS_ = "127.0.0.1#8053";
