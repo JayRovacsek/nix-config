f03b345248c060e62232f84b6791e217321f4ac6
Jay Rovacsek <jay@rovacsek.com>
2022-03-09 23:51:34 +1100

    Remove passwd and attempt to use uid/gid

--- modules/docker/configs/pihole.nix
+++ modules/docker/configs/pihole.nix
@@ -3,11 +3,8 @@ in rec {
   image = "pihole/pihole:latest";
   serviceName = "pihole";
   ports = [ "53:53/tcp" "53:53/udp" "67:67/udp" "80:80/tcp" "444:443/tcp" ];
-  volumes = [
-    "/etc/pihole:/etc/pihole:rw"
-    "/etc/passwd:/etc/passwd:ro"
-    "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw"
-  ];
+  volumes =
+    [ "/etc/pihole:/etc/pihole:rw" "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw" ];
   environment = {
     TZ = "Australia/Sydney";
     PIHOLE_DNS_ = "127.0.0.1#8053";
@@ -22,10 +19,6 @@ in rec {
   extraOptions = [
     "--network=podman"
     "--name=${serviceName}"
-    ''
-      --user="${builtins.toString piholeUserConfig.uid}:${
-        builtins.toString piholeUserConfig.group.id
-      }"''
     "--cap-add=CAP_NET_BIND_SERVICE"
   ];
 }
