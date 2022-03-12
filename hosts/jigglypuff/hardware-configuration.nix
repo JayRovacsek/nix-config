919224213d79f23ec65910725395c12e9033581a
Jay Rovacsek <jay@rovacsek.com>
2022-03-12 14:21:57 +1100

    Add extra boot options for pi3 as it doesn't have a hardware overlay

--- hosts/jigglypuff/hardware-configuration.nix
+++ hosts/jigglypuff/hardware-configuration.nix
@@ -5,4 +5,15 @@
       fsType = "ext4";
     };
   };
+
+  boot.loader.grub.enable = false;
+
+  boot.loader.raspberryPi = {
+    enable = true;
+    version = 3;
+  };
+
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
