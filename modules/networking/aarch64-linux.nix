fbb1c1d8e119e573c7d772913b01a34fa6dd80b4
Jay Rovacsek <jay@rovacsek.com>
2022-03-12 15:18:55 +1100

    Add conditional logic to DHCP based on use of vlans

--- modules/networking/aarch64-linux.nix
+++ modules/networking/aarch64-linux.nix
@@ -1,1 +1,11 @@
-{ useDHCP = false; }
+{ config, ... }:
+let
+  hasVlans =
+    if (builtins.length (builtins.attrNames config.networking.vlans) != 0) then
+      true
+    else
+      false;
+in {
+  # useDHCP = false; 
+  useDHCP = !hasVlans;
+}
