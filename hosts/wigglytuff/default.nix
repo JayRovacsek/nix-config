622700f45f6c4331ed626494034d7458e0dca6f2
Jay Rovacsek <jay@rovacsek.com>
2022-03-12 20:23:54 +1100

    Add wireless back to Wigglytuff

--- hosts/wigglytuff/default.nix
+++ hosts/wigglytuff/default.nix
@@ -8,6 +8,13 @@ in {
   imports =
     [ ./hardware-configuration.nix ./modules.nix ./system-packages.nix ];
 
+  networking.wireless.environmentFile = "/run/secrets/wireless.env";
+  networking.wireless = {
+    enable = true;
+    interfaces = [ "wlan0" ];
+    networks."@IOT_SSID@".psk = "@IOT_PSK@";
+  };
+
   networking.hostName = "wigglytuff";
   networking.hostId = "d2a7b80b";
 }
