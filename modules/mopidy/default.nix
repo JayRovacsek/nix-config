47c3bb1e04573df8a9c375e38db7fee337ef62d3
Jay Rovacsek <jay@rovacsek.com>
2022-03-12 23:37:45 +1100

    Swap to iris

--- modules/mopidy/default.nix
+++ modules/mopidy/default.nix
@@ -3,7 +3,7 @@ let secrets = (builtins.fromJSON (builtins.readFile /secrets/jellyfin.json));
 in {
   services.mopidy = {
     enable = true;
-    extensionPackages = with pkgs; [ mopidy-jellyfin mopidy-muse ];
+    extensionPackages = with pkgs; [ mopidy-jellyfin mopidy-iris ];
     configuration = ''
       [audio]
       mixer = none
@@ -23,8 +23,8 @@ in {
       port = 6680
       zeroconf = Mopidy HTTP Server
       csrf_protection = true
-      default_app = mopidy
-      [muse]
+      default_app = iris
+      [iris]
       enabled = true
       [file]
       enabled = false
