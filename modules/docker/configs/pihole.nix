3fa9580431dac415a55f5a36addffd3d8ef7e1c3
Jay Rovacsek <jay@rovacsek.com>
2022-03-13 14:03:36 +1100

    Add to local DNS config Remove DHCP from pi again Apply some extra config options to pihole Enable niceness and non-standard user

--- modules/docker/configs/pihole.nix
+++ modules/docker/configs/pihole.nix
@@ -2,23 +2,35 @@ let piholeUserConfig = import ../../../users/service-accounts/pihole.nix;
 in rec {
   image = "pihole/pihole:latest";
   serviceName = "pihole";
-  ports = [ "53:53/tcp" "53:53/udp" "67:67/udp" "80:80/tcp" ];
-  volumes =
-    [ "/etc/pihole:/etc/pihole:rw" "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw" ];
+  ports = [ "53:53/tcp" "53:53/udp" "80:80/tcp" ];
+  volumes = [
+    "/etc/pihole:/etc/pihole:rw"
+    "/etc/pihole/dnsmasq.d:/etc/dnsmasq.d:rw"
+    "/etc/passwd:/etc/passwd:ro"
+  ];
   environment = {
     TZ = "Australia/Sydney";
     PIHOLE_DNS_ = "127.0.0.1#8053";
     DNSSEC = "true";
-    DNS_BOGUS_PRIV = "false";
+    DNS_BOGUS_PRIV = "true";
     DNS_FQDN_REQUIRED = "true";
     DHCP_ACTIVE = "false";
     DNSMASQ_LISTENING = "all";
     DNSMASQ_USER = "pihole";
     WEBPASSWORD = "password";
+    WEBTHEME = "default-dark";
+    FTLCONF_RESOLVE_IPV6 = "no";
+    FTLCONF_SOCKET_LISTENING = "all";
+    FTLCONF_NAMES_FROM_NETDB = "true";
+    PIHOLE_UID = piholeUserConfig.uid;
+    PIHOLE_GID = piholeUserConfig.group.id;
   };
   extraOptions = [
     "--name=${serviceName}"
     "--cap-add=CAP_NET_BIND_SERVICE"
+    "--cap-add=CAP_SYS_NICE"
+    "--cap-add=CAP_CHOWN"
     "--network=host"
+    ''--user="${piholeUserConfig.uid}:${piholeUserConfig.group.id}"''
   ];
 }
--- modules/docker/stacks/dns/local.nix
+++ modules/docker/stacks/dns/local.nix
@@ -9,6 +9,7 @@ in {
     address=/speedtest.localdomain/192.168.1.222
     address=/duplicati.localdomain/192.168.1.223
     address=/tv.localdomain/192.168.3.2
+    address=/wigglytuff.localdomain/192.168.3.2
     address=/car_bed.localdomain/192.168.3.10
     address=/jackett.localdomain/192.168.4.129
     address=/deluge.localdomain/192.168.4.130
@@ -22,12 +23,15 @@ in {
     address=/flare-solverr.localdomain/192.168.4.138
     address=/swag.localdomain/192.168.5.3
     address=/pihole.localdomain/192.168.6.2
+    address=/stubby.localdomain/192.168.6.3
+    address=/jigglypuff.localdomain/192.168.6.4
     address=/authelia.localdomain/192.168.9.2
     address=/nextcloud.localdomain/192.168.10.2
     address=/homeassistant.localdomain/192.168.12.2
     address=/cache.localdomain/192.168.16.2
     address=/minecraft.localdomain/192.168.17.5
     address=/minecraft.rovacsek.com/192.168.17.5
+    address=/minecraft.rovacsek.com.localdomain/192.168.17.5
     address=/valheim.localdomain/192.168.17.3
     address=/valheim.rovacsek.com/192.168.17.3
     address=/terraria.localdomain/192.168.17.4
