900ad620907b63b8c1d4e57ccdf937f68625260d
Jay Rovacsek <jay@rovacsek.com>
2022-03-13 14:15:13 +1100

    Give up once again in dropping builtin users

--- modules/docker/configs/pihole.nix
+++ modules/docker/configs/pihole.nix
@@ -1,9 +1,7 @@
 let
   piholeUserConfig = import ../../../users/service-accounts/pihole.nix;
   piholeUid = builtins.toString piholeUserConfig.uid;
-  piholeUname = piholeUserConfig.name;
   piholeGid = builtins.toString piholeUserConfig.group.id;
-  piholeGname = piholeUserConfig.group.name;
 in rec {
   image = "pihole/pihole:latest";
   serviceName = "pihole";
@@ -36,6 +34,5 @@ in rec {
     "--cap-add=CAP_SYS_NICE"
     "--cap-add=CAP_CHOWN"
     "--network=host"
-    ''--user="${piholeUname}:${piholeGname}"''
   ];
 }
