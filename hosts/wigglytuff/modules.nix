63b86e69701b2e9f5f8ec04e7cef0b0b9e17357e
Jay Rovacsek <jay@rovacsek.com>
2022-05-19 21:11:19 +1000

    Add lorri to wigglytuff

--- hosts/wigglytuff/modules.nix
+++ hosts/wigglytuff/modules.nix
@@ -4,6 +4,7 @@
     ../../modules/fonts
     ../../modules/gnupg
     ../../modules/hardware/raspberry-pi-4
+    ../../modules/lorri
     ../../modules/networking
     ../../modules/nix
     ../../modules/openssh
