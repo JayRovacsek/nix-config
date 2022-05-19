130c2b559755fea285c9f4495a656423332db84e
Jay Rovacsek <jay@rovacsek.com>
2022-05-19 21:08:12 +1000

    Add autologin if the jay user exists

--- modules/xfce/default.nix
+++ modules/xfce/default.nix
@@ -1,7 +1,24 @@
-{
+{ config, pkgs, ... }:
+let
+  autoLoginEnable =
+    builtins.any (x: x.name == "jay") (builtins.attrValues config.users.users);
+  autoLogin = if autoLoginEnable then {
+    enable = true;
+    user = "jay";
+  } else
+    { };
+in {
   services.xserver = {
     enable = true;
-    displayManager.lightdm.enable = true;
-    desktopManager.xfce.enable = true;
+    displayManager = {
+      lightdm = {
+        inherit autoLogin;
+        enable = true;
+      };
+    };
+    desktopManager.xfce = {
+      enable = true;
+      enableScreensaver = false;
+    };
   };
 }
