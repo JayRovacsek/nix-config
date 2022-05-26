c72ae7288dd54b95ded3c701501f3caa02d47f8a
Jay Rovacsek <jay@rovacsek.com>
2022-05-27 09:12:20 +1000

    Remove commented key

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -3,6 +3,11 @@
     enable = true;
     permitRootLogin = "no";
     passwordAuthentication = false;
+    # The below just has to be _one_ of the private keys described in the secrets/secrets.nix module
+    hostKeys = [{
+      path = "/tmp/.ssh/id_ed25519_agenix";
+      type = "ed25519";
+    }];
   };
 
   networking.firewall.allowedTCPPorts = [ 22 ];
