c258e2555190bfa2d883ac0a737f6e0e732aa4f1
Jay Rovacsek <jay@rovacsek.com>
2022-05-26 22:36:59 +1000

    Add ssh key configs

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,4 +4,15 @@ let
   secondaryKey =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEovGIk8AE7nM76xwGc7AmryAX3EdoL5qAtEs2sItDE";
   keys = [ primaryKey secondaryKey ];
-in { "authelia_users_database.yaml.age".publicKeys = keys; }
+in {
+  "authelia_users_database.yaml.age".publicKeys = keys;
+  "jay_ssh_config.age".publicKeys = keys;
+  "id_ed25519_sk_type_a_1.age".publicKeys = keys;
+  "id_ed25519_sk_type_a_1.pub.age".publicKeys = keys;
+  "id_ed25519_sk_type_c_1.age".publicKeys = keys;
+  "id_ed25519_sk_type_c_1.pub.age".publicKeys = keys;
+  "id_ed25519_sk_type_a_2.age".publicKeys = keys;
+  "id_ed25519_sk_type_a_2.pub.age".publicKeys = keys;
+  "id_ed25519_sk_type_c_2.age".publicKeys = keys;
+  "id_ed25519_sk_type_c_2.pub.age".publicKeys = keys;
+}
