d1c7afb59e89a079860ff3c6cdddb64660f88e3b
Jay Rovacsek <jay@rovacsek.com>
2022-05-27 20:36:00 +1000

    WIP

--- functions/user.nix
+++ functions/user.nix
@@ -4,7 +4,7 @@
     initialHashedPassword = userConfig.initialHashedPassword;
     extraGroups = userConfig.extraGroups;
     openssh.authorizedKeys.keys = userConfig.openssh.authorizedKeys.keys;
-    home = "/home/${userConfig.name}";
+    home = userConfig.home;
     shell = pkgs."${userConfig.shell}";
   };
 }
--- users/standard/jay.nix
+++ users/standard/jay.nix
@@ -1,5 +1,7 @@
-{
-  name = "jay";
+{ config, pkgs, ... }:
+let isLinux = pkgs.stdenv.isLinux;
+in rec {
+  name = builtins.toString (if isLinux then "jay" else "jrovacsek");
   initialHashedPassword =
     "$6$LRvlOuUlmWfOtbKW$JuSDUvL0ykqAhFi80rMdWrc89wDz/uJ1Mt6WuHpsa/7kxSTWlz5O0f7xRvFvJ6nxEePUkxx/52FuHHl3rEhj61";
   extraGroups =
@@ -8,5 +10,46 @@
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAHcp9K2S8UZWpWTkuXLsEJQ57qoXpriEqHmU4AjrWKFAAAABHNzaDo= jay@rovacsek.com"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICOGbb7QMU4l5jccNIaGp9M8HBhyGm3JeNnD30td49nfAAAABHNzaDo= jay@rovacsek.com"
   ];
+  home = if isLinux then "/home/${name}" else "/Users/${name}";
   shell = "zsh";
+  # The below should just be done by gen list, then map against a + c but I'll fix it later
+  secrets = [
+    {
+      file = "id_ed25519_sk_type_a_1";
+      # file = ../../secrets/id_ed25519_sk_type_a_1.age;
+      path = "${home}/.ssh/id_ed25519_sk_type_a_1";
+    }
+    {
+      file = "id_ed25519_sk_type_a_2";
+      path = "${home}/.ssh/id_ed25519_sk_type_a_2";
+    }
+    {
+      file = "id_ed25519_sk_type_a_1_pub";
+      path = "${home}/.ssh/id_ed25519_sk_type_a_1.pub";
+    }
+    {
+      file = "id_ed25519_sk_type_a_2_pub";
+      path = "${home}/.ssh/id_ed25519_sk_type_a_2.pub";
+    }
+    {
+      file = "id_ed25519_sk_type_c_1";
+      path = "${home}/.ssh/id_ed25519_sk_type_c_1";
+    }
+    {
+      file = "id_ed25519_sk_type_c_2";
+      path = "${home}/.ssh/id_ed25519_sk_type_c_2";
+    }
+    {
+      file = "id_ed25519_sk_type_c_1_pub";
+      path = "${home}/.ssh/id_ed25519_sk_type_c_1.pub";
+    }
+    {
+      file = "id_ed25519_sk_type_c_2_pub";
+      path = "${home}/.ssh/id_ed25519_sk_type_c_2.pub";
+    }
+    {
+      file = "ssh_config";
+      path = "${home}/.ssh/config";
+    }
+  ];
 }
