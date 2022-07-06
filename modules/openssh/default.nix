2f15d4194d64f4ddc59a566d735bc31dd8949951
Jay Rovacsek <jay@rovacsek.com>
2022-07-07 00:02:01 +1000

    Fix issue with issuing ssh keys by dynamically importing ssh keys where possible

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -1,4 +1,22 @@
-{
+{ config, pkgs, lib, ... }:
+let
+  agenixPresent = builtins.hasAttr "age" config;
+
+  users = builtins.filter (x: x.isNormalUser)
+    (builtins.attrValues config.users.users);
+
+  sshKeys = builtins.foldl' (a: b: a // b) { } (builtins.map (user:
+    (builtins.foldl' (a: b: a // b) { } (builtins.map (x: {
+      "${lib.strings.removeSuffix ".age" x}" = {
+        file = ../../secrets/${x};
+        mode = "0400";
+        owner = config.services.headscale.user;
+      };
+    }) (builtins.filter (z: (lib.strings.hasInfix "id-ed25519" z))
+      (builtins.attrNames (builtins.readDir ../../secrets)))))) users);
+in {
+  age.secrets = sshKeys;
+
   services.openssh = {
     enable = true;
     permitRootLogin = "no";
