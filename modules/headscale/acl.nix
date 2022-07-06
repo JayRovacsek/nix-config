cd3a665ab7f3fe1c2fa4242d07abcbd91c08bb44
Jay Rovacsek <jay@rovacsek.com>
2022-07-06 23:38:46 +1000

    Move configs to meta file, augment acls to build some default values & required routes

--- modules/headscale/acl.nix
+++ modules/headscale/acl.nix
@@ -1,17 +1,33 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let
+  meta = import ./meta.nix { inherit config pkgs lib; };
+
+  # Below generates group values of "group:$X" for all pre-auth namespaces we've stored
+  groups = builtins.foldl' (x: y: x // y) { }
+    (builtins.map (x: { "group:${x}" = [ "${x}" ]; }) meta.namespaces);
+
+  # Below generates an allow ACL for inter-namespace communication where the namespace matches the origin
+  defaultNamespaceCommunication = builtins.map (x: {
+    action = "accept";
+    src = [ "${x}" ];
+    dst = [ "${x}:*" ];
+  }) meta.namespaces;
+
+  allowTrustToAll = [{
+    action = "accept";
+    src = [ "group:trust" ];
+    dst = [ "*:*" ];
+  }];
+
+  allowAllToDNS = [{
+    action = "accept";
+    src = [ "group:*" ];
+    dst = [ "group:dns:53,8053" ];
+  }];
+
   aclConfig = {
-    acls = [{
-      action = "accept";
-      src = [ "*" ];
-      dst = [ "*:*" ];
-    }];
-    ssh = [{
-      action = "check";
-      src = [ "autogroup:members" ];
-      dst = [ "autogroup:self" ];
-      users = [ "autogroup:nonroot" "root" ];
-    }];
+    inherit groups;
+    acls = allowTrustToAll ++ allowAllToDNS ++ defaultNamespaceCommunication;
   };
 in {
   environment.etc."headscale/acls.json" = {
--- modules/headscale/default.nix
+++ modules/headscale/default.nix
@@ -1,79 +1,10 @@
 { config, pkgs, lib, ... }:
-let
-  unixEpoch = "'1970-01-01 00:00:00.000000000+00:00'";
-  futureMeProblem = "'2050-01-01 00:00:00.000000000+00:00'";
-
-  # Okay, so the below is straigh pain to decipher, this is likely due to my nix 
-  # capabilities but translates as follows:
-  # * read the contents of our secrets directory, grabbing all file names
-  # * where the filename includes "tailscale" or "headscale"
-  # * map keys to a structure of ("name" minus ".age") = set representing agenix config
-  #
-  # So yeah, not pretty but scales to pull in all secrets that include the 
-  # "-preauth-key" value so we can generate entries into sqlite on every rebuild
-  secrets = builtins.foldl' (a: b: a // b) { } (builtins.map (x: {
-    "${lib.strings.removeSuffix ".age" x}" = {
-      file = ../../secrets/${x};
-      mode = "0400";
-      owner = config.services.headscale.user;
-    };
-  }) (builtins.filter (z:
-    (lib.strings.hasInfix "tailscale" z || lib.strings.hasInfix "headscale" z))
-    (builtins.attrNames (builtins.readDir ../../secrets))));
-
-  preauthKeys = builtins.filter (x: lib.strings.hasInfix "-preauth-key" x.name)
-    (builtins.attrValues config.age.secrets);
-
-  # CREATE TABLE `namespaces` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`name` text UNIQUE,PRIMARY KEY (`id`));
-  namespaceInsertStatements = builtins.concatStringsSep "\n" (lib.lists.imap0
-    (i: v:
-      "INSERT INTO namespaces ('id','created_at','updated_at','name') VALUES (${
-        builtins.toString i
-      }, ${unixEpoch},${unixEpoch},'${
-        lib.strings.removePrefix "tailscale-"
-        (lib.strings.removeSuffix "-preauth-key" v.name)
-      }');") preauthKeys);
-
-  # CREATE TABLE `pre_auth_keys` (`id` integer,`key` text,`namespace_id` integer,`reusable` numeric,`ephemeral` numeric DEFAULT false,`used` numeric DEFAULT false,`created_at` datetime,`expiration` datetime,PRIMARY KEY (`id`));
-  preauthInsertStatements = builtins.concatStringsSep "\n" (lib.lists.imap0
-    (i: v:
-      "INSERT INTO pre_auth_keys ('id','key','namespace_id','reusable','ephemeral','used','created_at','expiration') VALUES (${
-        builtins.toString i
-      },'`cat ${v.path}`',${
-        builtins.toString i
-      },1,0,0,${unixEpoch},${futureMeProblem});") preauthKeys);
-
-  # The delete from all tables below could probably be done better but I can't DB :)
-  # It's required to ensure we have a blank slate moving in and no residual state can be 
-  # left behind causing issues later
-  sqlStatement = ''
-    delete from namespaces;
-    delete from pre_auth_keys;
-    delete from machines;
-    delete from kvs;
-    delete from api_keys;
-
-    ${namespaceInsertStatements}
-
-    ${preauthInsertStatements}
-  '';
-
-  script = with pkgs; ''
-    bash -c "
-    sqlite3 ${config.services.headscale.database.path} <<'END_SQL'
-
-    ${sqlStatement}
-
-    END_SQL"
-  '';
-
-  imports = map (n: "${./pkgConfigs}/${n}")
-    (builtins.attrNames (builtins.readDir ./pkgConfigs));
+let meta = import ./meta.nix { inherit config pkgs lib; };
 in {
 
   imports = [ ./acl.nix ];
 
-  age.secrets = secrets;
+  age.secrets = meta.secrets;
 
   networking.firewall = {
     allowedTCPPorts = [ config.services.headscale.port ];
@@ -83,7 +14,7 @@ in {
   environment.systemPackages = with pkgs; [ headscale sqlite-interactive ];
 
   systemd.services."headscale-autosetup" = {
-    inherit script;
+    script = meta.script;
 
     description = "Automatic configuration of Headscale";
 
