7e50272b45e5d95894f112908a47b655ffc7c2b6
Jay Rovacsek <jay@rovacsek.com>
2022-07-07 13:14:35 +1000

    Minor fixes to tailscale config

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -19,13 +19,14 @@ let
     (builtins.attrNames (builtins.readDir ../../secrets))));
 
   preauthPath = config.age.secrets."tailscale-${tailnet}-preauth-key".path;
+
+  hostname = config.networking.hostName;
 in {
 
   age.secrets = secrets;
 
   # Client tailscale config
   services.tailscale = {
-    port = 0;
     enable = true;
     interfaceName = "tailscale0";
   };
@@ -37,35 +38,38 @@ in {
     checkReversePath = "loose";
   };
 
+  systemd.services.tailscaled.wants = [ "tailscaled.service" ];
+
   # create a job to authenticate to Tailscale
   # thanks to https://github.com/ashkan-leo/machines/blob/main/modules/network/tailscale.nix for this
   systemd.services."tailscale-autoconnect" = {
     description = "Automatic connection to Tailscale";
 
     # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
+    after = [ "tailscaled.service" ];
+    wantedBy = [ "tailscaled.service" ];
 
-    serviceConfig.Type = "exec";
-
-    path = with pkgs; [ bash tailscale ];
+    serviceConfig.Type = "oneshot";
 
     script = ''
-      bash -c "
       # wait for tailscaled to settle
       sleep 2
 
       # check if we are already authenticated to tailscale
       status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
 
-      if [ $status == "Running" ]; then # Assume changes might have been made to this config - re-auth to ensure we match our config
+      if [ $status = "Running" ]; then
         tailscale logout
       fi
 
       sleep 2
 
-      ${pkgs.tailscale}/bin/tailscale up -authkey $(cat ${preauthPath}) --login-server ${loginServer}"
+      if [ -f "${preauthPath}" ]; then
+        ${pkgs.tailscale}/bin/tailscale up \
+        --auth-key=file:${preauthPath} \
+        --login-server=${loginServer} \
+        --hostname=${hostname}
+      fi
     '';
   };
 }
