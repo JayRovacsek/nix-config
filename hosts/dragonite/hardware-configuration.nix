77627c6446d08e2da8b0176eb009883ebf672a7c
Jay Rovacsek <jay@rovacsek.com>
2022-07-13 19:41:06 +1000

    Add binfmt emulated system to dragonite to speed up cross compiles

--- hosts/dragonite/hardware-configuration.nix
+++ hosts/dragonite/hardware-configuration.nix
@@ -4,6 +4,8 @@
   powerManagement.enable = false;
 
   boot = {
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
+
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
