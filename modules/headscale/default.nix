02e61d963b0352df0ec08c8127a433500640d087
Jay Rovacsek <jay@rovacsek.com>
2022-07-14 15:25:47 +1000

    Changes to headscale server - generate some TLS keys to test if these are needed

--- modules/headscale/default.nix
+++ modules/headscale/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }:
-let meta = import ./meta.nix { inherit config pkgs lib; };
+let
+  meta = import ./meta.nix { inherit config pkgs lib; };
+  derpServerStunPort = 3478;
+  metricsPort = 9090;
+  grpcPort = 50443;
 in {
 
   imports = [ ./acl.nix ];
@@ -33,53 +37,11 @@ in {
   };
 
   services.headscale = {
-    settings = {
-      metrics_listen_addr = "127.0.0.1:9090";
-      grpc_listen_addr = "0.0.0.0:50443";
-      grpc_allow_insecure = false;
-      private_key_path = "/var/lib/headscale/private.key";
-      ip_prefixes = [ "100.64.0.0/10" ];
-      disable_check_updates = true;
-      randomize_client_port = false;
-    };
     enable = true;
+    port = 8080;
+    package = pkgs.headscale;
     address = "0.0.0.0";
     serverUrl = "https://headscale.rovacsek.com";
-    dns = {
-      magicDns = true;
-      # Replace this in time with resolved magic DNS address of my DNS resolvers.
-      nameservers = [ "1.1.1.1" ];
-      domains = [ "rovacsek.com.internal" ];
-      baseDomain = "rovacsek.com.internal";
-    };
-    logLevel = "info";
-    ## TODO: Address the below to use my own options.
-    # see also: https://github.com/kradalby/dotfiles/blob/bfeb24bf2593103d8e65523863c20daf649ca656/machines/headscale.oracldn/headscale.nix#L45
-    # derp = {
-    #   urls = [];
-    #   paths = [];
-    # updateFrequency
-    # autoUpdate
-    # };
-    # TODO: make this dynamic depending on a search through /etc configs for this system
-    aclPolicyFile = "/etc/headscale/acls.json";
-    # I can see the below being problematic while still using SWAG.
-    # Will need to dig into changing this once I can extract SWAG into nixified modules.
-    # tls.letsencrypt = {
-    #   certFile = "";
-    #   keyFile = ""; # Both require a path on host to existing valid certs
-    # };
-    # Looks like the below will require uplift to authelia 
-    # see: https://www.authelia.com/docs/configuration/identity-providers/oidc.html#openid-connect
-    # openIdConnect = {
-    #   issuer = "";
-    #   domainMap = { };
-    #   clientId = "headscale";
-    #   clientSecretFile = config.age.secrets.headscale-oidc-secret.path;
-    # };
-    # I don't think the below is really required just yet, a default of 
-    # 30 mins seems just fine
-    # ephemeralNodeInactivityTimeout = "5m";
 
     database = {
       user = "headscale";
@@ -87,6 +49,57 @@ in {
       path = "/var/lib/headscale/db.sqlite";
       name = "headscale";
       passwordFile = config.age.secrets.headscale-db-password.path;
+    };
+
+    dns = {
+      magicDns = true;
+      # Replace this in time with resolved magic DNS address of my DNS resolvers.
+      nameservers =
+        [ "192.168.6.4" "192.168.6.2" "igglybuff.dns.rovacsek.com.internal" ];
+      domains = [ "rovacsek.com.internal" ];
+      baseDomain = "rovacsek.com.internal";
+      # More settings for this in services.headscale.settings as they currently aren't mapped in nix module
+    };
+
+    privateKeyFile = config.age.secrets.headscale-private-key.path;
+    logLevel = "debug";
+    ## TODO: Address the below to use my own options.
+    # see also: https://github.com/kradalby/dotfiles/blob/bfeb24bf2593103d8e65523863c20daf649ca656/machines/headscale.oracldn/headscale.nix#L45
+    derp = {
+      # TODO: Remove below once I have paths correctly configured
+      # urls = [ ];
+      # paths = [ "/etc/headscale/derp-server.json" ];
+      updateFrequency = "24h";
+      autoUpdate = true;
+    };
+
+    tls = {
+      certFile = config.age.secrets."headscale-tls-crt".path;
+      keyFile = config.age.secrets."headscale-tls-key".path;
+    };
+
+    # TODO: make this dynamic depending on a search through /etc configs for this system
+    aclPolicyFile = "/etc/headscale/acls.json";
+
+    ephemeralNodeInactivityTimeout = "5m";
+
+    # This will override settings that are not exposed as nix module options
+    settings = {
+      metrics_listen_addr = "127.0.0.1:${builtins.toString metricsPort}";
+      grpc_listen_addr = "127.0.0.1:${builtins.toString grpcPort}";
+      #   grpc_allow_insecure = false;
+      ip_prefixes = [ "100.64.0.0/10" ];
+
+      # Enable headscale to act as DERP
+      derp = {
+        server = {
+          enabled = true;
+          region_id = 999;
+          region_code = "fap";
+          region_name = "headscale.rovacsek.com";
+          stun_listen_addr = "0.0.0.0:${builtins.toString derpServerStunPort}";
+        };
+      };
     };
   };
 }
