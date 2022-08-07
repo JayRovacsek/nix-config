ca27d70e9806bf16d939f688b84b647386dfc4dd
Jay Rovacsek <jay@rovacsek.com>
2022-08-08 09:18:21 +1000

    Apply patches to latest nvidia drivers

--- modules/nvidia/nvfbc-unlock.nix
+++ modules/nvidia/nvfbc-unlock.nix
@@ -170,6 +170,8 @@ let
       "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x00\\x72\\x08\\x48/";
     "515.57" =
       "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x00\\x72\\x08\\x48/";
+    "515.65.01" =
+      "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x00\\x72\\x08\\x48/";
   };
 in driverPackage.overrideAttrs ({ version, preFixup ? "", ... }: {
   preFixup = preFixup + ''
