4c51f338b0fda1edfe2e9f08ad7e839aba44e945
Jay Rovacsek <jay@rovacsek.com>
2022-08-17 23:53:57 +1000

    Bloody darwin with garbage ssh

--- packages/x86_64-darwin.nix
+++ packages/x86_64-darwin.nix
@@ -6,6 +6,11 @@
     git
     htop
     jq
+    # This is required as the version of SSH on darwin is garbage and doesn't support
+    # yubikeys properly.
+    # Adding open ssh will mean it ranks higher in path leading to a suitable SSH
+    # package to back both SSH and git
+    openssh
 
     # Need to work on the below - but this _should_ be in shell.nix but vscode doesn't work this way just yet.
     nodejs
