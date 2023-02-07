1d57ec7a1aef4f80dd18fe09c90846574c81ac1e
Jay Rovacsek <jay@rovacsek.com>
2023-02-08 09:11:03 +1100

    Simplify some elements of code Add sudo module

--- hosts/dragonite/default.nix
+++ hosts/dragonite/default.nix
@@ -24,31 +24,14 @@ in {
   age.identityPaths =
     [ "/agenix/id-ed25519-ssh-primary" "/agenix/id-ed25519-headscale-primary" ];
 
-  virtualisation.oci-containers.backend = "docker";
-
   services.tailscale.tailnet = "admin";
 
-  security.sudo.wheelNeedsPassword = false;
-
-  ## Todo: write out the below - need to rework networking module.
-  networking = {
-    wireless.enable = false;
-    hostId = "acd009f4";
-    hostName = "dragonite";
-    useDHCP = false;
-    interfaces.enp9s0.useDHCP = true;
-
-    firewall = {
-      ## Todo: remove below as they can be abstracted into microvms
-      # For reference:
-      # 5900: VNC (need to kill)
-      # 8200: Duplicati
-      allowedTCPPorts = [ 5900 8200 ];
-    };
-  };
-
-  imports =
-    [ ./hardware-configuration.nix ./modules.nix ./system-packages.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ./modules.nix
+    ./networking.nix
+    ./system-packages.nix
+  ];
 
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
