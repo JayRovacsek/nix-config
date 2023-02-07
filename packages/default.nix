eb902569f6e1762055265f3f9b76b813b84bf5ba
Jay Rovacsek <jay@rovacsek.com>
2023-02-07 21:53:27 +1100

    :rip: do need the hack to contextually include packages

--- packages/default.nix
+++ packages/default.nix
@@ -6,16 +6,33 @@ let
     self.inputs.stable.legacyPackages.${system};
 
   inherit (pkgs) callPackage;
+  inherit (pkgs.stdenv) isLinux isDarwin isx86_64;
   inherit (pkgs.lib) recursiveUpdate;
 
-  packages = {
+  # Amethyst only evaluates on darwin, because the context of this is both
+  # via NUR and a flake, the below is done to hack this requirement
+  darwinPackages = if isDarwin then {
     amethyst = callPackage ./amethyst { };
-    better-english = callPackage ./better-english { };
     velociraptor-bin = callPackage ./velociraptor-bin { };
-    netextender = callPackage ./netextender { };
+  } else
+    { };
+
+  # As above, velociraptor isn't build upstream for aarch.
+  # would be cool to resolve this.
+  x64LinuxPackages = if isLinux && isx86_64 then {
+    velociraptor-bin = callPackage ./velociraptor-bin { };
+  } else
+    { };
+
+  extraPackages = darwinPackages // x64LinuxPackages;
+
+  packages = {
+    better-english = callPackage ./better-english { };
+    # Disable until I can resolve this
+    # netextender = callPackage ./netextender { };
     trdsql-bin = callPackage ./trdsql-bin { };
     vulnix-pre-commit = callPackage ./vulnix-pre-commit { };
-  };
+  } // extraPackages;
 
   # Understand the context of where we're at - if in a flake then
   # merge the raspberry pi images in also to be a possible build option.
