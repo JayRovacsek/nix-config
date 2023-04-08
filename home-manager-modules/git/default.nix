29d6901eb9d409390484cd35b2b9f375d3715133
Jay Rovacsek <jay@rovacsek.com>
2023-04-08 20:13:47 +1000

    Fix git module assuming signing etc is available

--- home-manager-modules/git/default.nix
+++ home-manager-modules/git/default.nix
@@ -1,4 +1,29 @@
-{ config, osConfig, ... }: {
+{ config, osConfig, lib, ... }:
+let
+  # Check if signing key is configured as expected
+  signingKeyConfigured = builtins.hasAttr "age" osConfig
+    && builtins.hasAttr "git-signing-key.pub" osConfig.age.secrets;
+
+  # Check if allowed signers file is configured as expected
+  allowedSignersFileConfigured =
+    builtins.hasAttr ".ssh/allowed_signers" config.home.file;
+
+  # Are both of the above true?
+  useExtraConfig = signingKeyConfigured && allowedSignersFileConfigured;
+
+  # Create a set with signing config present for systems that 
+  # can support it
+  extraConfig = lib.attrsets.optionalAttrs useExtraConfig {
+    commit.gpgsign = true;
+    gpg = {
+      format = "ssh";
+      ssh.allowedSignersFile =
+        config.home.file.".ssh/allowed_signers".source.outPath;
+    };
+    user.signingkey = osConfig.age.secrets."git-signing-key.pub".path;
+  };
+
+in {
   programs.git = {
     enable = true;
     difftastic = {
@@ -13,16 +38,6 @@
     userEmail = "jay@rovacsek.com";
     userName = "jayrovacsek";
 
-    # TODO: make the below optional for settings in which we don't want to 
-    # deploy git signing keys
-    extraConfig = {
-      commit.gpgsign = true;
-      gpg = {
-        format = "ssh";
-        ssh.allowedSignersFile =
-          config.home.file.".ssh/allowed_signers".source.outPath;
-      };
-      user.signingkey = osConfig.age.secrets."git-signing-key.pub".path;
-    };
+    inherit extraConfig;
   };
 }
