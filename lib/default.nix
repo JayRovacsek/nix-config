5fb4e73bbde3185666a231d64bf485862fe8566b
Jay Rovacsek <jay@rovacsek.com>
2023-04-12 23:14:33 +1000

    Add hacky parse at SBOM schema (lots of cleanup to do)

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,5 @@
 { self }: {
+  generate-sbom = import ./generate-sbom.nix { inherit self; };
   generate-user-config = import ./generate-user-config.nix { inherit self; };
   home-manager = import ./home-manager.nix { inherit self; };
   merge-user-config = import ./merge-user-config.nix { inherit self; };
