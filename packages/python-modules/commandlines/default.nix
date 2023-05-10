5baeb0d107663bd9c12cd43fd3078270cdaf2568
jayrovacsek <jay@rovacsek.com>
2023-05-11 08:03:24 +1000

    Implement backgroundremover

---
+++ packages/python-modules/commandlines/default.nix
@@ -0,0 +1,28 @@
+{ lib, fetchPypi, python, ... }:
+let
+  pname = "commandlines";
+  name = pname;
+  version = "0.4.1";
+
+  meta = with lib; {
+    description =
+      "Commandlines is a Python library for command line application development that supports command line argument parsing";
+    platforms = platforms.all;
+    homepage = "https://github.com/chrissimpkins/commandlines";
+    downloadPage = "https://github.com/chrissimpkins/commandlines/releases";
+    license = licenses.mit;
+  };
+
+  inherit (python) buildPythonPackage;
+
+in buildPythonPackage {
+  inherit pname name version meta;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-hrZQt4RwrJWWbXsanSFcFlkbzLNLKK4ruQJsO0Fm/WQ=";
+  };
+
+  doCheck = false;
+
+}
