5c4588f5b6b60a23ffdbf5cb2885191cceaad900
Jay Rovacsek <jay@rovacsek.com>
2023-05-21 11:23:20 +1000

    Simplify code handling base images Move AWS + OCI terraform globals out to it's own file Add scaffold AMI code for terraform (incorrect - to be reworked)

--- common/images/default.nix
+++ common/images/default.nix
@@ -3,41 +3,45 @@ let
   inherit (self.lib) merge;
   inherit (self) nixosConfigurations;
   inherit (self.inputs) nixos-generators;
-  inherit (self.inputs.nixpkgs.lib) optional;
-  inherit (self.inputs.nixpkgs.lib.attrsets) filterAttrs mapAttrsToList;
   inherit (nixosConfigurations) rpi1 rpi2;
 
   sd-configurtations = [ rpi1 rpi2 ];
-  # TODO: the below addition of amazon is causing issues with 
-  # generating linode images meaning we've borked the cloud-based-images
-  # definition below.
-  # Need to resolve the issue, move a majority of the code to lib.
-  cloud-formats = [ "amazon" "linode" "qcow" ];
 
-  cloud-base-images = builtins.foldl'
-    (accumulator: set: (builtins.listToAttrs set) // accumulator) { }
-    (mapAttrsToList (name: value:
-      builtins.foldl' (accumulator: format:
-        [{
-          name = "${format}-base-image";
-          value = let
-            # Hack required due to this issue: https://github.com/nix-community/nixos-generators/issues/150
-            inherit (value._module.args) modules;
-            extraModules = optional (format == "amazon")
-              (_: { amazonImage.sizeMB = 16 * 1024; });
-          in nixos-generators.nixosGenerate {
-            inherit format;
-            inherit (value.pkgs.stdenv) system;
-            modules = modules ++ extraModules;
-          };
-        }] ++ accumulator) [ ] cloud-formats
+  amazon-base-image = let
+    inherit (self.nixosConfigurations) amazon;
+    inherit (amazon._module.args) modules;
+  in nixos-generators.nixosGenerate {
+    system = "x86_64-linux";
+    modules = modules ++ [{ amazonImage.sizeMB = 16 * 1024; }];
+    format = "amazon";
+  };
 
-    ) (filterAttrs (name: _: builtins.elem name cloud-formats)
-      nixosConfigurations));
+  linode-base-image = let
+    inherit (self.nixosConfigurations) linode;
+    inherit (linode._module.args) modules;
+  in nixos-generators.nixosGenerate {
+    system = "x86_64-linux";
+    inherit modules;
+    format = "linode";
+  };
 
-  # Create a list of identifier to sdImage build derivations.
+  oracle-base-image = let
+    inherit (self.nixosConfigurations) oracle;
+    inherit (oracle._module.args) modules;
+  in nixos-generators.nixosGenerate {
+    system = "x86_64-linux";
+    inherit modules;
+    format = "qcow";
+  };
+
   sd-images = builtins.map (image: {
     "${image.config.networking.hostName}" = image.config.system.build.sdImage;
   }) sd-configurtations;
 
-in merge ([ cloud-base-images ] ++ sd-images)
+  cloud-images = {
+    inherit amazon-base-image linode-base-image oracle-base-image;
+  };
+
+  images = [ cloud-images ] ++ sd-images;
+
+in merge images
