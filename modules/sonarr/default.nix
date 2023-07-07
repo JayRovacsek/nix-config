8af1347486ad5a027d7a79575f7a529fe1730e65
Jay Rovacsek <jay@rovacsek.com>
2023-07-08 09:21:51 +1000

    Minor work adding config options to sonarr

--- modules/sonarr/default.nix
+++ modules/sonarr/default.nix
@@ -1,30 +1,36 @@
 { config, ... }:
 let
-  cfg = config.flake.lib.generators.to-xml {
+
+  cfg = {
     Config = {
-      LogLevel = "info";
-      EnableSsl = "False";
-      Port = 8989;
-      SslPort = 9898;
+      LogLevel = config.services.sonarr.logLevel;
+      EnableSsl = if config.services.sonarr.enableSsl then "True" else "False";
+      Port = config.services.sonarr.port;
+      SslPort = config.services.sonarr.sslPort;
       BindAddress = "*";
       # The below is not a live key. It will be changed prior to deploy
       ApiKey = "85e1526d459348f8a92d3b1a7f67286f";
-      AuthenticationMethod = "None";
+      AuthenticationMethod = config.services.sonarr.authenticationMethod;
       UpdateMechanism = "BuiltIn";
       Branch = "main";
       InstanceName = "Sonarr";
     };
   };
+
+  cfg-text = config.flake.lib.generators.to-xml cfg;
 in {
+  imports = [ ../../options/sonarr ];
+
   services.sonarr = {
     enable = true;
-    openFirewall = true;
     dataDir = "/etc/sonarr";
+    openPort = true;
+    port = 9999;
   };
 
   environment.etc."sonarr/config.xml" = {
     inherit (config.services.sonarr) user group;
-    text = cfg;
+    text = cfg-text;
     mode = "0750";
   };
 }
---
+++ options/sonarr/default.nix
@@ -0,0 +1,69 @@
+{ config, lib, ... }:
+let cfg = config.services.sonarr;
+in with lib; {
+  options.services.sonarr = {
+    openPort = mkOption {
+      type = with types; bool;
+      default = false;
+      description = "";
+    };
+
+    openSslPort = mkOption {
+      type = with types; bool;
+      default = false;
+      description = "";
+    };
+
+    openAllPorts = mkOption {
+      type = with types; bool;
+      default = false;
+      description = "";
+    };
+
+    port = mkOption {
+      type = with types; int;
+      default = 8989;
+      description = "";
+    };
+
+    sslPort = mkOption {
+      type = with types; int;
+      default = 9898;
+      description = "";
+    };
+
+    logLevel = mkOption {
+      type = with types; enum [ "fatal" "error" "warn" "info" "debug" ];
+      default = 9898;
+      description = "";
+    };
+
+    authenticationMethod = mkOption {
+      type = with types; enum [ "None" "Basic" "Forms" ];
+      default = "None";
+      description = "";
+    };
+
+    enableSsl = mkOption {
+      type = with types; bool;
+      default = false;
+      description = "";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Add some smarts behind if we open any, some or all of the 
+    # config ports.
+    # This is a hack around the fact upstream hardcodes the port configuration
+    # option as 8989
+    networking.firewall = mkIf
+      (builtins.any (x: x) [ cfg.openPort cfg.openSslPort cfg.openAllPorts ]) {
+        allowedTCPPorts = if cfg.openAllPorts then [
+          cfg.port
+          cfg.sslPort
+        ] else
+          (lib.optional cfg.openPort cfg.port)
+          ++ (lib.optional cfg.openSslPort cfg.sslPort);
+      };
+  };
+}
