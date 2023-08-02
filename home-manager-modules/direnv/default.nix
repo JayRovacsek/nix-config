a98d6a8876e6f651a7b5d97cdeb25344aad105bc
Jay Rovacsek <jay@rovacsek.com>
2023-08-03 09:04:32 +1000

    Implement nom for all systems

--- home-manager-modules/direnv/default.nix
+++ home-manager-modules/direnv/default.nix
@@ -1,12 +1,11 @@
 { config, ... }: {
   programs.direnv = {
     enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
     config = {
-      global = { load_dotenv = true; };
-      whitelist = {
-        prefix =
-          [ "/home/jay/dev" "/Users/j.rovacsek/dev" "/Users/jrovacsek/dev" ];
-      };
+      global.load_dotenv = true;
+      whitelist.prefix = [ "${config.home.homeDirectory}/dev" ];
     };
   };
 }
