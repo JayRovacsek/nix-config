71951c6ebc3fd9d5c1fd163d2e31db99666fc97f
jayrovacsek <jay@rovacsek.com>
2023-08-11 16:00:32 +1000

    Implement stub broot module

---
+++ home-manager-modules/broot/default.nix
@@ -0,0 +1,19 @@
+{ config, osConfig, ... }:
+let
+  enable = true;
+
+  enableBashIntegration = config.programs.bash.enable
+    && osConfig.programs.bash.enable;
+  enableFishIntegration = config.programs.fish.enable
+    && osConfig.programs.fish.enable;
+  enableZshIntegration = config.programs.zsh.enable
+    && osConfig.programs.zsh.enable;
+
+  settings.modal = true;
+
+in {
+  programs.broot = {
+    inherit enable enableBashIntegration enableFishIntegration
+      enableZshIntegration settings;
+  };
+}
