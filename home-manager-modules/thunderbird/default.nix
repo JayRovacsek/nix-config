afc9307969da71acdb50de1324927b29c8b8b018
jayrovacsek <jay@rovacsek.com>
2023-08-11 16:36:00 +1000

    Minor fixes for thunderbird

--- home-manager-modules/thunderbird/default.nix
+++ home-manager-modules/thunderbird/default.nix
@@ -2,15 +2,17 @@
 let
   inherit (pkgs.stdenv) isLinux;
 
-  cfg = lib.optionalAttrs isLinux programs.thunderbird {
-    enable = true;
-    profiles.jay = {
-      isDefault = true;
-      settings = { };
-    };
-    settings = {
-      "general.useragent.override" = "";
-      "privacy.donottrackheader.enabled" = true;
+  cfg = lib.optionalAttrs isLinux {
+    programs.thunderbird = {
+      enable = true;
+      profiles.jay = {
+        isDefault = true;
+        settings = { };
+      };
+      settings = {
+        "general.useragent.override" = "";
+        "privacy.donottrackheader.enabled" = true;
+      };
     };
   };
 in cfg
