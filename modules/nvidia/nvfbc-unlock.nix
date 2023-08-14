d47364652e4100c34dfc060db27fe54ad0d5394e
Jay Rovacsek <jay@rovacsek.com>
2023-08-14 20:51:48 +1000

    Bump nvidia patches

--- modules/nvidia/nvfbc-unlock.nix
+++ modules/nvidia/nvfbc-unlock.nix
@@ -199,6 +199,10 @@ let
       "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x00\\x72\\x08\\x48/";
     "535.86.05" =
       "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x01\\x90\\x90\\x48/";
+    "535.86.10" =
+      "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x01\\x90\\x90\\x48/";
+    "535.98" =
+      "s/\\x83\\xfe\\x01\\x73\\x08\\x48/\\x83\\xfe\\x01\\x90\\x90\\x48/";
   };
 in driverPackage.overrideAttrs ({ version, preFixup ? "", ... }: {
   preFixup = preFixup + ''
