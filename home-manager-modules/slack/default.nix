b1712e6503617dcc2097b524ee31ba466cc57956
Jay Rovacsek <jay@rovacsek.com>
2023-09-09 08:46:48 +1000

    Further fixes for slack on aarch

--- home-manager-modules/slack/default.nix
+++ home-manager-modules/slack/default.nix
@@ -3,5 +3,5 @@ let
   # TODO: rewrite this garbage
   enable = !(lib.hasInfix "aarch" osConfig.nixpkgs.system);
 
-  packages = lib.optional enable (with pkgs; [ slack ]);
+  packages = lib.optionals enable (with pkgs; [ slack ]);
 in { home = { inherit packages; }; }
