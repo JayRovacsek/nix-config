a12501e8d5474a1b8a588f05f25d57c005716c77
Jay Rovacsek <jay@rovacsek.com>
2023-09-09 09:46:08 +1000

    Implement MVP of nix flake check for local run that should mimic github actions mostly

--- apps/act.nix
+++ apps/act.nix
@@ -2,17 +2,10 @@
 let
   inherit (pkgs) act;
 
-  cfg = pkgs.writeText "flatpak-shim-info" config;
-
-  config = ''
-    -P ubuntu-latest=node:16-buster-slim
-    -P ubuntu-22.04=node:16-buster-slim
-    -P ubuntu-20.04=node:16-buster-slim
-    -P ubuntu-18.04=node:16-buster-slim
-  '';
+  args = [ "-P ubuntu-latest=alpine:latest" "-j validate-nix" ];
 
   program = builtins.toString (pkgs.writers.writeBash "run-act" ''
-    ${act}/bin/act --env-file ${cfg}
+    ${act}/bin/act ${builtins.concatStringsSep " " args}
   '');
 
   type = "app";
