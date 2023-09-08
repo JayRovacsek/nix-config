8680d62003195084e0f372b6a1d515e4d830bace
Jay Rovacsek <jay@rovacsek.com>
2023-09-09 08:30:37 +1000

    Change slack to not load on aarch systems

--- home-manager-modules/slack/default.nix
+++ home-manager-modules/slack/default.nix
@@ -1,1 +1,7 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ slack ]; }
+{ pkgs, lib, osConfig, ... }:
+let
+  # TODO: rewrite this garbage
+  enable = !(lib.hasInfix "aarch" osConfig.nixpkgs.system);
+
+  packages = lib.optional enable (with pkgs; [ slack ]);
+in { home = { inherit packages; }; }
