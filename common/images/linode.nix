4d642266b1388617d473707107de8649f3073bd0
Jay Rovacsek <jay@rovacsek.com>
2023-11-07 20:43:37 +1100

    Explicitly add disable-assertions to cloud images

--- common/images/linode.nix
+++ common/images/linode.nix
@@ -1,11 +1,12 @@
 { self }:
 let
   inherit (self.common.system) unstable-system;
+  inherit (self.common.assertions) disable-assertions;
   inherit (self.nixosModules) linode-image zsh;
   inherit (self.common.package-sets) x86_64-linux-unstable;
 
   inherit (x86_64-linux-unstable) system identifier pkgs;
   base = self.common.modules.${identifier};
-  modules = base ++ [ ../../hosts/ditto linode-image zsh ];
+  modules = base ++ [ ../../hosts/ditto disable-assertions linode-image zsh ];
 
 in unstable-system { inherit system pkgs modules; }
