4290f56ca21d3631f2bf32340122c274773a6fc0
jayrovacsek <jay@rovacsek.com>
2023-11-24 19:13:07 +1100

    Maybe needed?

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -4,9 +4,30 @@ _: {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
   };
 
   security.rtkit.enable = true;
 
   hardware.pulseaudio.enable = false;
+
+  # Disable select outputs - below the following devices are disabled:
+  # * HDMI screen output on alakazam
+  environment.etc."wireplumber/main.lua.d/51-alsa-disable.lua" = {
+    mode = "0444";
+    text = ''
+      rule = {
+        matches = {
+          {
+            { "device.name", "equals", "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1" },
+          },
+        },
+        apply_properties = {
+          ["device.disabled"] = true,
+        },
+      }
+
+      table.insert(alsa_monitor.rules,rule)
+    '';
+  };
 }
