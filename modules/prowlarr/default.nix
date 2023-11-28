c55c221e14623563ff599cda7e9d76d9b42ab502
jayrovacsek <jay@rovacsek.com>
2023-11-28 15:27:51 +1100

    WIP services implementation

--- modules/prowlarr/default.nix
+++ modules/prowlarr/default.nix
@@ -1,6 +1,29 @@
-_: {
-  services.prowlarr = {
-    enable = true;
-    openFirewall = true;
+{ config, ... }:
+let
+  inherit (config.flake.lib.nginx) generate-domains generate-vhosts;
+
+  service-name = "prowlarr";
+
+  domains = generate-domains { inherit config service-name; };
+
+  virtualHosts = generate-vhosts {
+    inherit config service-name;
+    # port = config.services.prowlarr.ports.http;
+    port = 9696;
+  };
+in {
+  # Extended options for nginx
+  # TODO: map prowlarr settings to custom options
+  imports = [ ../../options/nginx ];
+  services = {
+    nginx = {
+      test = { inherit domains; };
+      inherit virtualHosts;
+    };
+
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
