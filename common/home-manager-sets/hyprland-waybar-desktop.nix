d96977f8706ea04d4f49306aaf8af53069b92621
jayrovacsek <jay@rovacsek.com>
2023-12-01 00:02:47 +1100

    Add mako back into waybar config

--- common/home-manager-sets/hyprland-waybar-desktop.nix
+++ common/home-manager-sets/hyprland-waybar-desktop.nix
@@ -2,4 +2,4 @@
 let
   inherit (self.common) home-manager-modules;
   inherit (self.common.home-manager-module-sets) hyprland-desktop;
-in hyprland-desktop ++ (with home-manager-modules; [ waybar ])
+in hyprland-desktop ++ (with home-manager-modules; [ mako waybar ])
