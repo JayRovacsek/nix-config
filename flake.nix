1b96c4d4f354e54b5e075b97cb93f04c31164886
jayrovacsek <jay@rovacsek.com>
2023-12-07 08:47:02 +1100

    WIP refactor for home manager

--- flake.nix
+++ flake.nix
@@ -323,7 +323,7 @@
         nixosModules = self.common.nixos-modules;
 
         # Home manager user definitions for system consumption
-        homeManagerConfiguration = import ./users { inherit self; };
+        homeManagerConfigurations = import ./users { inherit self; };
 
         # Home manager modules for user consumption
         homeManagerModules = self.common.home-manager-modules;
