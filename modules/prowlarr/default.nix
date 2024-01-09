8cfdc015a9c687e31af6e134f6864384e0fe364b
jayrovacsek <jay@rovacsek.com>
2024-01-09 12:30:10 +1100

    Put prowlarr on a diet

--- modules/prowlarr/default.nix
+++ modules/prowlarr/default.nix
@@ -1,34 +1,6 @@
-{ config, ... }:
-let
-  inherit (config.flake.lib.nginx) generate-domains generate-vhosts;
-  inherit (config.flake.lib.authelia) generate-access-rules;
-
-  service-name = "prowlarr";
-
-  port = 9696;
-
-  domains = generate-domains { inherit config service-name; };
-
-  overrides.locations."~ (/prowlarr)?(/[0-9]+)?/api".proxyPass =
-    "http://localhost:${builtins.toString port}";
-
-  virtualHosts = generate-vhosts {
-    inherit config service-name overrides port;
-    # port = config.services.prowlarr.ports.http;
-  };
-in {
-  # Extended options for nginx
+_: {
   # TODO: map prowlarr settings to custom options
-  imports = [ ../../options/nginx ];
   services = {
-    authelia.instances =
-      generate-access-rules config.services.nginx.domains service-name;
-
-    nginx = {
-      test = { inherit domains; };
-      inherit virtualHosts;
-    };
-
     prowlarr = {
       enable = true;
       openFirewall = true;
