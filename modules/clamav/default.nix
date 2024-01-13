c139c1165cc6133824d8cb626ef96256f3d28e91
jayrovacsek <jay@rovacsek.com>
2024-01-13 15:01:44 +1100

    Augment current clamav options

--- modules/clamav/default.nix
+++ modules/clamav/default.nix
@@ -1,24 +1,38 @@
 { pkgs, lib, ... }: {
   # Inspiration: https://github.com/houstdav000/dotfiles/blob/733f2db667e3645619ca9dc05446099864045a49/nixos/config/services/clamav.nix
   services.clamav = {
-    updater = {
-      enable = true;
-      frequency = 24;
-      interval = "hourly";
-    };
     daemon = {
       enable = true;
       settings = {
+        DetectPUA = true;
+        ExtendedDetectionInfo = true;
+        FollowDirectorySymlinks = true;
+        FollowFileSymlinks = true;
         LogFile = "/var/log/clamd.log";
+        LogSyslog = true;
         LogTime = true;
+        MaxDirectoryRecursion = 30;
+        OnAccessExcludeUname = "clamav";
+        OnAccessExtraScanning = true;
+        OnAccessPrevention = true;
         VirusEvent = lib.escapeShellArgs [
           "${pkgs.libnotify}/bin/notify-send"
           "--"
           "ClamAV Virus Scan"
           "Found virus: %v"
         ];
-        DetectPUA = true;
       };
+    };
+
+    scanner = {
+      enable = true;
+      scanDirectories = [ "/etc" "/home" "/srv" "/tmp" "/var/lib" "/var/tmp" ];
+    };
+
+    updater = {
+      enable = true;
+      frequency = 24;
+      interval = "hourly";
     };
   };
   environment.systemPackages = with pkgs; [ libnotify ];
