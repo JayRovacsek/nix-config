dfb61ed5412fa2305326478ffd373fa69740b118
jayrovacsek <jay@rovacsek.com>
2024-01-27 17:56:16 +1100

    Give the reduced file-count treatment to a few more hosts

--- hosts/wigglytuff/default.nix
+++ hosts/wigglytuff/default.nix
@@ -21,26 +21,103 @@ in {
   inherit flake;
   inherit (merged) users home-manager;
 
-  imports = [
-    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
-    ./hardware-configuration.nix
-    ./wireless.nix
-  ];
+  imports = [ "${modulesPath}/installer/sd-card/sd-image-aarch64.nix" ];
 
-  age.identityPaths = [ "/agenix/id-ed25519-ssh-primary" ];
+  age = {
+    identityPaths = [
+      "/agenix/id-ed25519-ssh-primary"
+      "/agenix/id-ed25519-wireless-primary"
+    ];
+
+    secrets."wireless.env" = {
+      file = ../../secrets/wireless/wireless-iot.env.age;
+      mode = "0400";
+      symlink = false;
+    };
+  };
+
+  boot = {
+    # VC4 required for GPU hardware acceleration
+    initrd.availableKernelModules = [
+      "pcie_brcmstb"
+      "reset-raspberrypi"
+      "usb_storage"
+      "usbhid"
+      "vc4"
+      "xhci_pci"
+    ];
+
+    # Set the headphone jack as the default output
+    extraModprobeConfig = ''
+      options snd_bcm2835 enable_headphones=1
+    '';
+
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    kernelParams = [
+      "cma=256M"
+      "console=tty1"
+      "console=ttyAMA0,115200"
+      "8250.nr_uarts=1"
+      "kunit.enable=0"
+    ];
+  };
 
   environment.systemPackages = with pkgs; [ jellyfin-media-player ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  hardware = {
+    # Filter for just pi4 replated device drivers
+    deviceTree.filter = "bcm2711-rpi-4*.dtb";
+
+    # Audio settings to ensure the headphones are the default
+    pulseaudio = {
+      enable = true;
+      # Default to the 3.5mm jack as output
+      extraConfig = ''
+        set-default-sink alsa_output.platform-bcm2835_audio.stereo-fallback.2
+      '';
+      package = pkgs.pulseaudioFull;
+    };
+
+    raspberry-pi."4" = {
+      # Enable GPU acceleration
+      fkms-3d = {
+        enable = true;
+        cma = 1024;
+      };
+    };
+  };
 
   networking = {
     hostName = "wigglytuff";
     hostId = "d2a7b80b";
+    wireless = {
+      enable = true;
+      environmentFile = config.age.secrets."wireless.env".path;
+      interfaces = [ "wlan0" ];
+      networks."@SSID@".psk = "@PSK@";
+    };
   };
+
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   services = {
     journald.storage = "volatile";
     # Hide Builder user from SDDM login
     xserver.displayManager.sddm.settings.Users.HideUsers = "builder";
   };
+
+  swapDevices = [{
+    device = "/swapfile";
+    size = 3072;
+  }];
 
   system.stateVersion = "24.05";
 }
