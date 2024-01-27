772c8e4e791510acdc039a86c274f64fc873a85f
jayrovacsek <jay@rovacsek.com>
2024-01-27 17:20:44 +1100

    Remove imports for jigglypuff, change journald to volatile storage given it's on an SD card

--- hosts/jigglypuff/default.nix
+++ hosts/jigglypuff/default.nix
@@ -15,8 +15,6 @@ in {
   inherit flake;
   inherit (merged) users home-manager;
 
-  imports = [ ./network.nix ];
-
   age = {
     secrets = {
       "git-signing-key" = rec {
@@ -55,10 +53,50 @@ in {
 
   hardware.enableRedistributableFirmware = true;
 
+  networking = {
+    hostName = "jigglypuff";
+    hostId = "d2a7f613";
+    # We're only got a single interface on the pi, so it'll be fine
+    # for now to just assume eth0
+    usePredictableInterfaceNames = false;
+  };
+
+  services.journald.storage = "volatile";
+
   swapDevices = [{
     device = "/swapfile";
     size = 1024;
   }];
 
-  system.stateVersion = "22.05";
+  systemd.network = {
+    netdevs."00-vlan-dns" = {
+      enable = true;
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-dns";
+      };
+      vlanConfig.Id = 6;
+    };
+
+    networks = {
+      "00-wired" = {
+        enable = true;
+        # mkForce is utilised here to override the default systemd
+        # settings in the systemd-networkd module
+        matchConfig.Name = lib.mkForce "eth0";
+        networkConfig = lib.mkForce {
+          VLAN = config.systemd.network.netdevs."00-vlan-dns".netdevConfig.Name;
+        };
+        dhcpV4Config = lib.mkForce { };
+      };
+
+      "10-vlan-dns" = {
+        enable = true;
+        name = config.systemd.network.netdevs."00-vlan-dns".netdevConfig.Name;
+        networkConfig.DHCP = "ipv4";
+      };
+    };
+  };
+
+  system.stateVersion = "24.05";
 }
