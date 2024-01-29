d2cc1c935ece958ad2dac6d00b5212ff41451ab0
jayrovacsek <jay@rovacsek.com>
2024-01-29 23:52:06 +1100

    WIP

--- packages/node/attack-workbench-frontend/default.nix
+++ packages/node/attack-workbench-frontend/default.nix
@@ -9,6 +9,8 @@ let
     description =
       " An application allowing users to explore, create, annotate, and share extensions of the MITRE ATT&CK® knowledge base. This repository contains an Angular-based web application providing the user interface for the ATT&CK Workbench application.";
     license = licenses.asl20;
+    # Until I can resolve angular cli & analytics
+    broken = true;
   };
 
   src = fetchFromGitHub {
@@ -18,9 +20,10 @@ let
     hash = "sha256-p0KuXtizDuBF5omoY7ypQ66HBpuMPEdhb+R3P3A8nI0=";
   };
 
-  sourceRoot = "app";
+  # Ugh - so source is needed here as per: https://github.com/NixOS/nixpkgs/issues/31172#issuecomment-341621518
+  sourceRoot = "source/app";
 
-  npmDepsHash = lib.fakeHash;
+  npmDepsHash = "sha256-Fdfto5n3oBWhyChXKyhjcvncbKIbWuNtBF2U+Pone6c=";
 
 in buildNpmPackage {
   inherit pname version src meta nodejs npmDepsHash sourceRoot;
