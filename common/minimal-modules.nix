dffaeb0855722f7c689d1b3c4437e8675e4070b8
Jay Rovacsek <jay@rovacsek.com>
2024-02-19 21:39:26 +1100

    Add impermanence to minimal module set

--- common/minimal-modules.nix
+++ common/minimal-modules.nix
@@ -1,11 +1,13 @@
 { self }:
 let
   inherit (self.common)
-    age gids options package-sets self-reference standardise-nix uids;
+    age gids impermanence options package-sets self-reference standardise-nix
+    uids;
   inherit (self.inputs.nur.nixosModules) nur;
 in builtins.mapAttrs (package-set: _: [
   age
   gids
+  impermanence.${package-set}
   options.${package-set}
   self-reference
   standardise-nix.${package-set}
