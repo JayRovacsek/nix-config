9ef258cfbcf3fe66b57b07237a5ef801e43dac8b
jayrovacsek <jay@rovacsek.com>
2024-07-29 08:56:03 +1000

    fix(linux): bump binarycache timeout on nidorina

--- hosts/nidorina/nginx.nix
+++ hosts/nidorina/nginx.nix
@@ -29,6 +29,7 @@ let
       enableAuthelia = false;
       locations."/" = {
         extraConfig = ''
+          proxy_connect_timeout 300;
           allow 10.0.0.0/8;
           allow 172.16.0.0/12;
           allow 192.168.0.0/16;
