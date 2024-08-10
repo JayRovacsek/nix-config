e05c2c965e5d6c2357934af4342aec54dcddb366
jayrovacsek <jay@rovacsek.com>
2024-08-10 18:32:56 +1000

    feat(images): start to minimise rpi1

--- common/images/rpi1.nix
+++ common/images/rpi1.nix
@@ -5,11 +5,15 @@ let
 in unstable-system {
   system = "armv6l-linux";
   modules = [
-    "${nixpkgs}/nixos/modules/profiles/base.nix"
+    # "${nixpkgs}/nixos/modules/profiles/base.nix"
     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image.nix"
     ({ config, lib, pkgs, ... }: {
       boot = {
+        bcache.enable = false;
+
         consoleLogLevel = lib.mkDefault 7;
+
+        enableContainers = false;
 
         # prevent `modprobe: FATAL: Module ahci not found`
         initrd.availableKernelModules = pkgs.lib.mkForce [ "mmc_block" ];
@@ -28,13 +32,32 @@ in unstable-system {
         };
       };
 
-      networking.hostName = "rpi1";
+      documentation = {
+        enable = false;
+        doc.enable = false;
+        info.enable = false;
+        man.enable = false;
+        nixos.enable = false;
+      };
+
+      fonts.fontconfig.enable = false;
+
+      networking = {
+        firewall.enable = false;
+        hostName = "rpi1";
+      };
 
       nixpkgs = {
         config.allowUnsupportedSystem = true;
         crossSystem = lib.systems.elaborate lib.systems.examples.raspberryPi;
         overlays = [ self.overlays.armv6l-fixes ];
       };
+
+      programs = {
+        nano.enable = false;
+        less.enable = lib.mkDefault false;
+      };
+
       system.stateVersion = "24.05";
 
       sdImage = {
@@ -44,6 +67,12 @@ in unstable-system {
           ${installBootLoader} ${toplevel} -d ./firmware
         '';
         firmwareSize = 64;
+      };
+
+      xdg = {
+        icons.enable = false;
+        mime.enable = false;
+        sounds.enable = false;
       };
     })
   ];
