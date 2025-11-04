509573259bbb66bb401818de539d364fa810c686
jayrovacsek <jay@rovacsek.com>
2025-11-05 09:43:56 +1100

    fix(iac,linux,modules): address nginx issues related to nextcloud

--- hosts/nidorina/nginx.nix
+++ hosts/nidorina/nginx.nix
@@ -284,28 +284,36 @@ let
     inherit (nextcloud) subdomain;
     overrides = {
       enableAuthelia = false;
-      # The below is required as by default nginx will utilise differing
-      # max client body sizes - this is simply a copy of the recommended
-      # nextcloud proxy config, minus any headers as the response from
-      # the second nginx proxy will add these headers
-      #
-      # Why do we need a dual reverse proxy config, to avoid needing to rewrite large
-      # elements of the upstream opinions on nextcloud.
-      extraConfig = ''
-        index index.php index.html /index.php$request_uri;
 
-        client_max_body_size 10G;
-        fastcgi_buffers 64 4K;
-        fastcgi_hide_header X-Powered-By;
-        gzip on;
-        gzip_vary on;
-        gzip_comp_level 4;
-        gzip_min_length 256;
-        gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
-        gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
-      '';
       locations."/" = {
-        extraConfig = "";
+        # The below is required as by default nginx will utilise differing
+        # max client body sizes - this is simply a copy of the recommended
+        # nextcloud proxy config, minus any headers as the response from
+        # the second nginx proxy will add these headers
+        #
+        # Why do we need a dual reverse proxy config, to avoid needing to rewrite large
+        # elements of the upstream opinions on nextcloud.
+        extraConfig = ''
+          index index.php index.html /index.php$request_uri;
+
+            proxy_buffering off;
+            proxy_hide_header Referrer-Policy;
+            proxy_hide_header X-Content-Type-Options;
+            proxy_hide_header X-Frame-Options;
+            proxy_hide_header X-XSS-Protection;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Real-IP $remote_addr;
+
+          client_max_body_size 10G;
+          fastcgi_buffers 64 4K;
+          fastcgi_hide_header X-Powered-By;
+          gzip on;
+          gzip_vary on;
+          gzip_comp_level 4;
+          gzip_min_length 256;
+          gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+          gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+        '';
         proxyPass = "${nextcloud.protocol}://${nextcloud.ipv4}:${builtins.toString nextcloud.port}";
       };
     };
