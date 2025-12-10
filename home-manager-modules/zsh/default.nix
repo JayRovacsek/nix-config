d58f5eb199d4624f8631bb93d60b2505f5067476
jayrovacsek <jay@rovacsek.com>
2025-12-10 17:30:30 +1100

    fix(home-manager-modules): vastly improve zsh load time

--- home-manager-modules/zsh/default.nix
+++ home-manager-modules/zsh/default.nix
@@ -19,6 +19,18 @@ in
     enableCompletion = false;
     enableVteIntegration = true;
 
+    initContent = lib.mkOrder 550 ''
+      DISABLE_AUTO_UPDATE="true"
+      DISABLE_COMPFIX="true"
+
+      setopt extendedglob
+      autoload -Uz compinit
+      for dump in ~/.zcompdump(N.mh+24); do
+        compinit
+      done
+      compinit -C
+    '';
+
     oh-my-zsh = {
       enable = true;
       plugins = [
@@ -48,6 +60,16 @@ in
         file = "you-should-use.plugin.zsh";
         name = "zsh-you-should-use";
         src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
+      }
+      {
+        name = "zsh-async";
+        src = pkgs.fetchFromGitHub {
+          owner = "mafredri";
+          repo = "zsh-async";
+          rev = "ee1d11b68c38dec24c22b1c51a45e8a815a79756";
+          hash = "sha256-mkTmgSnfoZlTaOPhw0Y0yG1BkZv+oTo/eEisZZTN5nM=";
+        };
+        file = "async.plugin.zsh";
       }
     ];
 
