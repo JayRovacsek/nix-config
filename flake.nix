5d9f4ddea3b455e06ff132ce4bd651dd6ff9cb88
jayrovacsek <jay@rovacsek.com>
2025-12-17 07:24:01 +1100

    chore: bump inputs to new stable and unstable versions

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Stable / Unstable split in packages
     bleeding-edge.url = "github:nixos/nixpkgs";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    stable.url = "github:nixos/nixpkgs/release-25.05";
+    stable.url = "github:nixos/nixpkgs/release-25.11";
 
     agenix = {
       inputs = {
