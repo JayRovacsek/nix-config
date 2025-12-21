645ffa59cd23ee0c801695d11e728108176aae94
jayrovacsek <jay@rovacsek.com>
2025-12-22 09:53:59 +1100

    fix(modules): apply overlay for nix-direnv with lix

--- modules/lix/default.nix
+++ modules/lix/default.nix
@@ -2,5 +2,16 @@
 {
   imports = [ self.inputs.lix-module.nixosModules.default ];
 
-  nix.package = pkgs.lix;
+  nixpkgs.overlays = [
+    (_final: prev: {
+      inherit (prev.lixPackageSets.stable)
+        nix-eval-jobs
+        nix-fast-build
+        nixpkgs-direnv
+        nixpkgs-review
+        ;
+    })
+  ];
+
+  nix.package = pkgs.lixPackageSets.stable.lix;
 }
