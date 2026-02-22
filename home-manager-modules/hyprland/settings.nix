14b8d556033a3a42c186c4ad24f5f4e964417e94
jayrovacsek <jay@rovacsek.com>
2026-02-22 15:34:34 +1100

    fix(linux): resolve display layouts on desktop

--- home-manager-modules/hyprland/settings.nix
+++ home-manager-modules/hyprland/settings.nix
@@ -41,7 +41,7 @@ let
     }
     # Position 3
     {
-      name = "DP-2";
+      name = "DP-3";
       resolution = "1920x1080";
       position = "3000x420";
       scale = "1";
@@ -49,7 +49,7 @@ let
     }
     # Position 4
     {
-      name = "DP-4";
+      name = "DP-5";
       resolution = "1920x1080";
       position = "4920x420";
       scale = "1";
