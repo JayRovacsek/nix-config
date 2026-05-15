591f1a6113c737248fd775c453e59bb20126d29e
jayrovacsek <jay@rovacsek.com>
2026-05-16 09:49:20 +1000

    refactor(modules): push lix through a range of downstreams

--- modules/lix/default.nix
+++ modules/lix/default.nix
@@ -3,5 +3,20 @@
   ...
 }:
 {
-  nix.package = pkgs.lixPackageSets.latest.lix;
+  nixpkgs.overlays = [
+    (final: prev: {
+      nix-monitored = prev.nix-monitored.override {
+        nix = final.lixPackageSets.latest.lix;
+        withNotify = pkgs.stdenv.isLinux;
+      };
+      nixos-rebuild = prev.nixos-rebuild.override {
+        nix = final.nix-monitored;
+      };
+      nix-direnv = prev.nix-direnv.override {
+        nix = final.nix-monitored;
+      };
+    })
+  ];
+
+  nix.package = pkgs.nix-monitored;
 }
