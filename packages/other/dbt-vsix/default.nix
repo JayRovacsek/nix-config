d66189e7a43398954558c00a72ce1b7dbad6f6b2
jayrovacsek <jay@rovacsek.com>
2026-05-21 13:34:26 +1000

    fix(packages): modernise dbt vscode plugin

--- packages/other/dbt-vsix/default.nix
+++ packages/other/dbt-vsix/default.nix
@@ -3,7 +3,7 @@ pkgs.vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "dbt";
     publisher = "dbtLabsInc";
-    version = "0.23.3";
-    hash = "sha256-NJwCikke6ixfFT+Vttkl8H5l+AxBq7rnzci42En9RBw=";
+    version = "0.73.0";
+    hash = "sha256-LiEc09GmE44RlJ+GwCn4cgIymcaBKoACHBUajfJ7Q8s=";
   };
 }
