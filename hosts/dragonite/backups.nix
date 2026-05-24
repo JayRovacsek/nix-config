8686e0870e55e2f3dd7fc775f6fde8091a65f201
jayrovacsek <jay@rovacsek.com>
2026-05-24 12:05:35 +1000

    refactor(iac): remove wasabi backups

--- hosts/dragonite/backups.nix
+++ hosts/dragonite/backups.nix
@@ -37,53 +37,6 @@ let
       }
     ];
   };
-
-  logs-wasabi-backup = {
-    logs-wasabi-backup = merge [
-      defaults
-      rec {
-        backupPrepareCommand = "${pkgs.restic}/bin/restic -r ${repository} unlock";
-        environmentFile = config.age.secrets.wasabi-backup-env.path;
-        extraOptions = [
-          "limit-upload=4096"
-          "compression=max"
-        ];
-        passwordFile = config.age.secrets.logs-password.path;
-        paths = [ "/srv/logs" ];
-        repository = "s3:https://s3.ap-southeast-2.wasabisys.com/sduhk02qkjfwuhoc6onreor4a99dhp0v";
-        timerConfig = {
-          OnCalendar = "hourly";
-          Persistent = true;
-        };
-      }
-    ];
-  };
-
-  nextcloud-wasabi-backup = {
-    nextcloud-wasabi-backup = merge [
-      defaults
-      rec {
-        backupPrepareCommand = "${pkgs.restic}/bin/restic -r ${repository} unlock";
-        environmentFile = config.age.secrets.wasabi-backup-env.path;
-        extraOptions = [
-          "limit-upload=4096"
-          "compression=max"
-        ];
-        passwordFile = config.age.secrets.nextcloud-password.path;
-        paths = [
-          "/srv/nextcloud"
-          "/var/lib/${builtins.hashString "md5" "nidoking"}/nextcloud"
-          "/var/lib/${builtins.hashString "md5" "nidoking"}/mysql"
-        ];
-        repository = "s3:https://s3.ap-southeast-2.wasabisys.com/sduhk02qkjfwuhoc6onreor4a99dhp0u";
-        timerConfig = {
-          OnCalendar = "00:05";
-          Persistent = true;
-          RandomizedDelaySec = "5h";
-        };
-      }
-    ];
-  };
 in
 {
 
@@ -99,8 +52,6 @@ in
   };
 
   services.restic.backups = merge [
-    logs-wasabi-backup
     nextcloud-local-disk-backup
-    nextcloud-wasabi-backup
   ];
 }
