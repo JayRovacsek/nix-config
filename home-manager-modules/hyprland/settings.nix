c08e977410a63585dd2bfba661c8406a918918bf
jayrovacsek <jay@rovacsek.com>
2026-07-25 08:26:04 +1000

    chore(home-manager-modules): update screens

--- home-manager-modules/hyprland/settings.nix
+++ home-manager-modules/hyprland/settings.nix
@@ -33,7 +33,7 @@ let
     }
     # Position 2
     {
-      name = "DP-5";
+      name = "DP-4";
       resolution = "1920x1080";
       position = "1920x0";
       scale = "1";
@@ -41,7 +41,7 @@ let
     }
     # Position 3
     {
-      name = "DP-3";
+      name = "DP-5";
       resolution = "1920x1080";
       position = "3840x0";
       scale = "1";
