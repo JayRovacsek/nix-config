3925ae48ab90c5bb2fc4a1540128f8888e9720aa
jayrovacsek <jay@rovacsek.com>
2026-07-25 18:37:48 +1000

    fix(modules): move rpis to community version module

--- modules/raspberry-pi-4/default.nix
+++ modules/raspberry-pi-4/default.nix
@@ -4,6 +4,7 @@
 }:
 {
   imports = [
+    self.inputs.nixos-raspberrypi.nixosModules.raspberry-pi-4.base
     self.nixosModules.minimal-boot-filesystems
   ];
 
--- modules/raspberry-pi-5/default.nix
+++ modules/raspberry-pi-5/default.nix
@@ -3,8 +3,8 @@
   ...
 }:
 {
-  imports = with self.inputs; [
-    nixos-hardware.nixosModules.raspberry-pi-5
+  imports = [
+    self.inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
     self.nixosModules.minimal-boot-filesystems
   ];
 
