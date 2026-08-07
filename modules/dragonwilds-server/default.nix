266c2daccb7a423f1d3df35ae10aa48cbcd0157f
jayrovacsek <jay@rovacsek.com>
2026-08-08 07:56:37 +1000

    feat(modules): add full dragonwilds config to module

--- modules/dragonwilds-server/default.nix
+++ modules/dragonwilds-server/default.nix
@@ -1,5 +1,15 @@
-_: {
-  imports = [ ../../options/modules/dragonwilds-server ];
+{ config, self, ... }: {
+  imports = [
+    ../../options/modules/dragonwilds-server
+    self.nixosModules.agenix
+  ];
 
-  services.dragonwilds-server.enable = true;
+  age.secrets.dragonwilds-server-config.file = ../../secrets/dragonwilds/server-config-file.age;
+
+  nixpkgs.config.allowUnfree = true;
+
+  services.dragonwilds-server = {
+    enable = true;
+    serverConfig = config.age.secrets.dragonwilds-server-config.path;
+  };
 }
